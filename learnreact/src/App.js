5ccc254efefa18123969e75e7dd02827364fdd7e
fengjing <fengjing@fengjingdeMacBook-Pro.local>
2019-07-17 16:52:54 +0800

    Styled_Components  与Reset.css初始化

--- learnreact/src/App.js
+++ learnreact/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { GlobalStyle } from './style'
 
 class App extends Component {
   render() {
     return (
-      <div>hello world</div>
+      <div>
+        <GlobalStyle/>
+        <div>hello aa</div>
+      </div>
     )
   }
 }
