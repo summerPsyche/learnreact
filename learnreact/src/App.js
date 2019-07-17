45205b720b78e47380dd6ee7d3dd623b7733ae77
fengjing <fengjing@fengjingdeMacBook-Pro.local>
2019-07-17 18:30:21 +0800

    feat: Header

--- learnreact/src/App.js
+++ learnreact/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { GlobalStyle } from './style'
+import Header from './common/header/index'
 
 class App extends Component {
   render() {
     return (
       <div>
         <GlobalStyle/>
-        <div>hello aa</div>
+        <Header/>
       </div>
     )
   }
